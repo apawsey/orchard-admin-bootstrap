966efd81caede2a74ac6538a9ff673ca9548fb4e
Jasmin Savard <jasmin.savard@gmail.com>
2014-08-10 12:20:20 -0400

    First commit

---
+++ Handlers/AdminThemeSiteSettingsPartHandler.cs
@@ -0,0 +1,25 @@
+﻿using JetBrains.Annotations;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.Handlers;
+using Orchard.Localization;
+using Admin.Bootstrap.Models;
+
+namespace Admin.Bootstrap.Handlers {
+    [UsedImplicitly]
+    public class AdminThemeSiteSettingsPartHandler : ContentHandler {
+        public AdminThemeSiteSettingsPartHandler() {
+            T = NullLocalizer.Instance;
+            Filters.Add(new ActivatingFilter<AdminThemeSettingsPart>("Site"));
+            Filters.Add(new TemplateFilterForPart<AdminThemeSettingsPart>("AdminThemeSettings", "Parts/Theme.AdminThemeSettings", "admin theme"));
+        }
+
+        public Localizer T { get; set; }
+
+        protected override void GetItemMetadata(GetContentItemMetadataContext context) {
+            if (context.ContentItem.ContentType != "Site")
+                return;
+            base.GetItemMetadata(context);
+            context.Metadata.EditorGroupInfo.Add(new GroupInfo(T("Admin Theme")));
+        }
+    }
+}
